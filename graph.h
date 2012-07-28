c9882e4bb2bdcf8a0e2ad3e75fcd22e6cf8d27b7
huangkevin57 <huangkevin57@yahoo.com>
2012-07-27 21:12:33 -0300

    Included structure updates (m, degrees, diameter, max_k, sum)

--- graph.h
+++ graph.h
@@ -6,11 +6,17 @@
 //graph.c adds stuff to infinity (crazy, I know),
 //So make sure it won't overflow
 #define GRAPH_INFINITY ((int)1000000)
-//#define MAXN 1000
+#define MAX_K (int(3))
+#define MAXN (int(10))
 
 typedef struct {
 	int *distances; //INFINITY for no connection
 	int n; //number of vertices
+	int sum_of_distances;
+	int m;
+	int *k;
+	int diameter;
+	int max_k;
 } distance_matrix;
 
 void floyd_warshall(distance_matrix g);
